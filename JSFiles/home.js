1ea6a1b5218076585770273a1ab6d99391a833cf
redrupm <146797557+redrupm@users.noreply.github.com>
2024-12-27 20:05:10 -0600

    slideshow complete!

--- JSFiles/home.js
+++ JSFiles/home.js
@@ -1,6 +1,4 @@
 let slideIndex = 1;
-showSlides(slideIndex);
-
 // Next/previous controls
 function slides(n) {
     showSlides(slideIndex += n);
@@ -8,16 +6,19 @@ function slides(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+}
+
+window.onload = () => {
+    showSlides(slideIndex);
 }
